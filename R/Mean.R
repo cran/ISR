de5a20bbf2c14935759ea24155355a00838c8514
Guangbao Guo <ggb11111111@163.com>
2022-04-22 11:00:03 +0000

    version 2022.4.22

--- R/Mean.R
+++ R/Mean.R
@@ -1,11 +1,11 @@
-#' Caculate the estimator on the mean method
+#' Caculate the estimator on the Mean method
 #'
 #' @param data is the orignal data set
 #' @param data0 is the missing data set
 #' @param real is to judge whether the data set is a real missing data set
 #' @param example is to judge whether the data set is a simulation example.
 #' 
-#' @return Xmean, MSEmean, MAEmean, REmean, GCVmean
+#' @return XMean, MSEMean, MAEMean, REMean, GCVMean
 #' @export
 #'
 #' @examples 
@@ -15,10 +15,10 @@
 #'  m=round(per*n*p,digits=0)
 #'  mr=sample(1:(n*p),m,replace=FALSE)
 #'  X0[mr]=NA;data0=X0
-#'  mean(data=data,data0=data0,real=FALSE,example=FALSE)
+#'  Mean(data=data,data0=data0,real=FALSE,example=FALSE)
 
-#the mean method 
-mean=function(data=0,data0,real=TRUE,example=FALSE)
+#the Mean method 
+Mean=function(data=0,data0,real=TRUE,example=FALSE)
 #It defaults that the data set is a real data set
 {#1
   if (real||example){#2
@@ -34,31 +34,31 @@ mean=function(data=0,data0,real=TRUE,example=FALSE)
   ina=as.matrix(mr%%n)
   jna=as.matrix(floor((mr+n-1)/n))
   data0[is.na(data0)]=cm0[ceiling(which(is.na(X0))/n)]	
-  Xmean=Xnew=as.matrix(data0)     
+  XMean=Xnew=as.matrix(data0)     
   for (j in 1:p){#2
      Mj=is.na(X0[,j])
      iob=which(Mj==FALSE)
      chj=sum(abs(round(X0[iob,j])-X0[iob,j]))
      if (chj==0){#3
-       Xmean[,j]=round(Xmean[,j])
+       XMean[,j]=round(XMean[,j])
      }else{#3
-       Xmean[,j]= Xmean[,j]
+       XMean[,j]= XMean[,j]
      }#3
   }#2
   if(real){#2
-    MSEmean= MAEmean= REmean='NULL'
+    MSEMean= MAEMean= REMean='NULL'
   }else{#2
-    MSEmean=(1/m)*t(Xnew[mr]-data[mr])%*%(Xnew[mr]-data[mr])
-    MAEmean=(1/m)*sum(abs(Xnew[mr]-data[mr]))	
-    REmean=(sum(abs(data[mr]-Xnew[mr])))/(sum(data[mr]))
+    MSEMean=(1/m)*t(Xnew[mr]-data[mr])%*%(Xnew[mr]-data[mr])
+    MAEMean=(1/m)*sum(abs(Xnew[mr]-data[mr]))	
+    REMean=(sum(abs(data[mr]-Xnew[mr])))/(sum(data[mr]))
   }#2
-  lambdamean=svd(cor(Xmean))$d
-  lmean=lambdamean/sum(lambdamean);J=rep(lmean,times=p);dim(J)=c(p,p)
+  lambdaMean=svd(cor(XMean))$d
+  lMean=lambdaMean/sum(lambdaMean);J=rep(lMean,times=p);dim(J)=c(p,p)
   upper.tri(J,diag=T);J[lower.tri(J)]=0;J;dim(J)=c(p,p)
-  etamean=matrix(colSums(J),nrow = 1,ncol = p,byrow = FALSE)
-  wwmean=which(etamean>=etatol);kmean=wwmean[1] 
-  lambdameanpk=lambdamean[(kmean+1):p]
-  GCVmean=sum(lambdameanpk)*p/(p-kmean)^2
-  return(list(Xmean=Xmean,MSEmean=MSEmean,MAEmean=MAEmean,REmean=REmean,GCVmean=GCVmean))
+  etaMean=matrix(colSums(J),nrow = 1,ncol = p,byrow = FALSE)
+  wwMean=which(etaMean>=etatol);kMean=wwMean[1] 
+  lambdaMeanpk=lambdaMean[(kMean+1):p]
+  GCVMean=sum(lambdaMeanpk)*p/(p-kMean)^2
+  return(list(XMean=XMean,MSEMean=MSEMean,MAEMean=MAEMean,REMean=REMean,GCVMean=GCVMean))
 }#1
 
